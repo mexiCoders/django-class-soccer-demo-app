6d312bffc585f34f8f2efa562c3bf4c69690f5a4
Johnny Dobbins <johnny.dobbins@gmail.com>
2015-08-04 08:44:07 -0500

    Update urls.py

--- soccer/urls.py
+++ soccer/urls.py
@@ -8,8 +8,6 @@ router = routers.DefaultRouter()
 router.register(r'players', PlayerViewSet)
 router.register(r'teams', TeamViewSet)
 
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     url(r'^', include(router.urls)),
 ]
